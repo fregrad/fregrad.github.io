e39a77dae0220be5614563ac8fffff23a3bfc5bf
Nguyen Dat <nguyendat@Nguyens-MacBook-Air.local>
2023-09-04 17:11:38 +0900

    [ADD] 6 iters samples

--- create_html_element.py
+++ create_html_element.py
@@ -1,24 +1,51 @@
 
-template = """<tbody>
+template = """
+              <tbody>
                 <tr>
                     <td align=\"center\"> 
-                      <audio id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples/GT/{filename}\"></audio>
+                      <audio id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/GT/{filename}\"></audio>
                     </td>
                     <td align=\"center\"> 
-                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples/wavegrad/{filename}\"></audio>
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/wavegrad/{filename}\"></audio>
                     </td>
                     <td align=\"center\"> 
-                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples/diffwave/{filename}\"></audio>
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/diffwave/{filename}\"></audio>
                     </td>
                     <td align=\"center\"> 
-                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples/priorgrad/{filename}\"></audio>
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/priorgrad/{filename}\"></audio>
                     </td>
                     <td align=\"center\"> 
-                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples/fregrad/{filename}\"></audio>
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/fregrad/{filename}\"></audio>
                     </td>
                 </tr>
               </tbody>"""
+import os
+filelist = os.listdir('static/FREGRAD_mos_samples_fast/diffwave')
+for f in filelist:
+    print(template.format(filename=f))
 
-filelist = [f.strip() for f in open("samples.txt")][:10]
+
+template = """
+              <tbody>
+                <tr>
+                    <td align=\"center\"> 
+                      <audio id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/GT/{filename}\"></audio>
+                    </td>
+                    <td align=\"center\"> 
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/wavegrad/{filename}\"></audio>
+                    </td>
+                    <td align=\"center\"> 
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/diffwave/{filename}\"></audio>
+                    </td>
+                    <td align=\"center\"> 
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/priorgrad/{filename}\"></audio>
+                    </td>
+                    <td align=\"center\"> 
+                      <audio  id=\"player\" controls=\"\"><source src=\"static/FREGRAD_mos_samples_fast/fregrad/{filename}\"></audio>
+                    </td>
+                </tr>
+              </tbody>"""
+import os
+filelist = os.listdir('static/FREGRAD_mos_samples_fast/diffwave')
 for f in filelist:
     print(template.format(filename=f))
